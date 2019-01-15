2fae5697fbdfbe29baee236d0ef1087e8e57cc2d
Steve Golden <sgolden2525@outlook.com>
2019-01-15 13:01:03 -0600

    CSS transitions on the left section
    Added JS with comments so that the "card-flip" is automatically initiated after a second.
    
    Added CSS with comments so that the animation/keyframe the svg left section moves ups, down, and then back to the middle.
    
    Removed the testing text on the left section on both divs.
    
    Removed the duck.svg.
    
    Changed fill color on mandala.svg to a dark blue

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 var card = document.querySelector('.card');
-card.addEventListener('click', function () {
+
+var testing = document.querySelector('body')
+
+/* This function adds the class which will "flip the card"  */
+function cardFlip(){
   card.classList.toggle('is-flipped');
-});
+}
+
+/* The setTimeOut function implements the cardFlip card with a one second delay */
+/* The function works with the animation/keyfrom in main.css */
+
+setTimeout(cardFlip, 1000);
